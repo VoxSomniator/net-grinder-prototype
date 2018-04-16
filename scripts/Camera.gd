cf42e8d4ba42268432a4cabcc54d7f79447b6016
Mercurial-Medley <mercurial.medley@gmail.com>
2018-04-16 01:23:17 -0700

    remove heading debug print

--- scripts/Camera.gd
+++ scripts/Camera.gd
@@ -13,5 +13,4 @@ func _process(delta):
 	heading = rad2deg(Vector2(cam_xform.basis.z.x, cam_xform.basis.z.z).angle_to(Vector2(0,1)))
 	if heading<0:
 		heading+=360 # beautify heading, remove negative angle
-	print(String(heading))
 	emit_signal("heading_updated", heading)
