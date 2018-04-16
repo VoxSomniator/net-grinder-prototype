96eaf2dad8a805e46dc1e3dbdf3e8dfbd7869f20
Mercurial-Medley <mercurial.medley@gmail.com>
2018-04-15 22:56:46 -0700

    add debug print to camera' signal, remove default comments

--- scripts/Camera.gd
+++ scripts/Camera.gd
@@ -5,11 +5,9 @@ signal heading_updated
 var heading
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
 	pass
 
 func _process(delta):
 	heading = self.rotation_degrees.y
-	
+	print("signal debug: Camera.gd")
 	emit_signal("heading_updated", heading)
