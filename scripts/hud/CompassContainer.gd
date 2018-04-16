12d42084191e64c76bad7d45b8ebf8cb8186cb40
Mercurial-Medley <mercurial.medley@gmail.com>
2018-04-15 22:29:24 -0700

    Connect heading signal to compass script, update compass script

--- scripts/hud/CompassContainer.gd
+++ scripts/hud/CompassContainer.gd
@@ -1,24 +1,16 @@
 extends Container
 
-#var tank_camera
-#var tank_cam_heading
-#var tank_cam_heading_int
+var heading
 
 func _ready():
-#	tank_camera = get_parent().get_node("Camera")
 	pass
 
 
-#func _process(delta):
-##	tank_cam_heading = tank_camera.rotation_degrees.y
-##	tank_cam_heading_int = int(tank_cam_heading)
-##
-##	$CompassTrack.rect_position.x = tank_cam_heading
+func _process(delta):
+	$CompassTrack.rect_position.x = heading
 
 func _draw():
 	VisualServer.canvas_item_set_clip(get_canvas_item(), true)
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _on_HUDTank_heading_updated(received_heading):
+	heading = recieved_heading
