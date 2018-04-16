0593d09d0b972dbe4ab867fb287b0c7080bdb4fd
Mercurial-Medley <mercurial.medley@gmail.com>
2018-04-15 22:57:58 -0700

    add line to emit signal, remove default comments

--- scripts/hud/HUD-Tank.gd
+++ scripts/hud/HUD-Tank.gd
@@ -2,18 +2,8 @@ extends Control
 
 signal heading_updated(heading)
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
 	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
-
 func _on_Camera_heading_updated(heading):
+	emit_signal("heading_updated", heading)
