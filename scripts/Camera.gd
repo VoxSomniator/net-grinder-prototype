ae475f33e30b9f7c58f42c03c8768efca19588fe
Mercurial-Medley <mercurial.medley@gmail.com>
2018-04-16 00:10:57 -0700

    fix variable name

--- scripts/Camera.gd
+++ scripts/Camera.gd
@@ -11,7 +11,7 @@ func _ready():
 func _process(delta):
 	cam_xform = self.get_global_transform()
 	heading = rad2deg(Vector2(cam_xform.basis.z.x, cam_xform.basis.z.z).angle_to(Vector2(0,1)))
-	if cam_heading<0:
-		cam_heading+=360 # beautify heading, remove negative angle
+	if heading<0:
+		heading+=360 # beautify heading, remove negative angle
 	print(String(heading))
 	emit_signal("heading_updated", heading)
