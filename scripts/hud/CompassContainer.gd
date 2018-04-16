b114b430b23def6854a6a687a5dc6349d0b20c20
Mercurial-Medley <mercurial.medley@gmail.com>
2018-04-15 22:33:45 -0700

    removed extra whitespace, fixed typo

--- scripts/hud/CompassContainer.gd
+++ scripts/hud/CompassContainer.gd
@@ -5,7 +5,6 @@ var heading
 func _ready():
 	pass
 
-
 func _process(delta):
 	$CompassTrack.rect_position.x = heading
 
@@ -13,4 +12,4 @@ func _draw():
 	VisualServer.canvas_item_set_clip(get_canvas_item(), true)
 
 func _on_HUDTank_heading_updated(received_heading):
-	heading = recieved_heading
+	heading = received_heading
