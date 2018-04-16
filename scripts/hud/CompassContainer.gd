9f7709450ae350c185a1f6268ebfd9e1f5c98865
Mercurial-Medley <mercurial.medley@gmail.com>
2018-04-15 21:39:59 -0700

    Comment out most previous compass scripting, pending rework
    Moving to signaling via parent HUD node

--- scripts/hud/CompassContainer.gd
+++ scripts/hud/CompassContainer.gd
@@ -1,17 +1,19 @@
 extends Container
 
-var tank_camera
-var tank_cam_heading
-var tank_cam_heading_int
+#var tank_camera
+#var tank_cam_heading
+#var tank_cam_heading_int
 
 func _ready():
-	tank_camera = get_parent().get_node("Camera")
+#	tank_camera = get_parent().get_node("Camera")
+	pass
 
-func _process(delta):
-	tank_cam_heading = tank_camera.rotation_degrees.y
-	tank_cam_heading_int = int(tank_cam_heading)
-	
-	$CompassTrack.rect_position.x = tank_cam_heading
+
+#func _process(delta):
+##	tank_cam_heading = tank_camera.rotation_degrees.y
+##	tank_cam_heading_int = int(tank_cam_heading)
+##
+##	$CompassTrack.rect_position.x = tank_cam_heading
 
 func _draw():
 	VisualServer.canvas_item_set_clip(get_canvas_item(), true)
