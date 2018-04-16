a60eabc868ef4bcd613f041c61d93bfbd37f4a9c
Mercurial-Medley <mercurial.medley@gmail.com>
2018-04-16 00:07:58 -0700

    add new variable for camera's global transform

--- scripts/Camera.gd
+++ scripts/Camera.gd
@@ -2,12 +2,14 @@ extends Camera
 
 signal heading_updated(heading)
 
+var cam_xform
 var heading
 
 func _ready():
 	pass
 
 func _process(delta):
-	heading = self.rotation_degrees.y
+	cam_xform = self.get_global_transform()
+	heading = cam_xform.rotation_degrees.y
 	print(String(heading))
 	emit_signal("heading_updated", heading)
