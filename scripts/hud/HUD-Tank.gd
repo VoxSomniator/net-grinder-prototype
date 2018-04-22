baad84b1b25fb346897e839464fcf6339d27dd25
Mercurial-Medley <mercurial.medley@gmail.com>
2018-04-21 19:13:13 -0700

    WIP PiP zoom

--- scripts/hud/HUD-Tank.gd
+++ scripts/hud/HUD-Tank.gd
@@ -5,6 +5,7 @@ signal body_heading_updated(body_heading)
 signal pitch_updated(pitch)
 signal turret_aimpoint_unprojected_updated(turret_aimpoint_unprojected)
 signal aimpoint_range_updated(aimpoint_range)
+signal pipcam_relayed(pipcam)
 
 func _ready():
 	pass
@@ -18,10 +19,11 @@ func _on_Player_body_heading_updated(body_heading):
 func _on_Camera_pitch_updated(pitch):
 	emit_signal("pitch_updated", pitch)
 
-
 func _on_Camera_turret_aimpoint_unprojected_updated(turret_aimpoint_unprojected):
 	emit_signal("turret_aimpoint_unprojected_updated", turret_aimpoint_unprojected)
 
-
 func _on_TurretAimPoint_aimpoint_range_updated(aimpoint_range):
 	emit_signal("aimpoint_range_updated", aimpoint_range)
+
+func _on_PIPZoomCamera_pipcam_relayed(pipcam):
+	emit_signal("pipcam_relayed")
